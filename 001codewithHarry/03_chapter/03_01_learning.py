a52996b75ee4271cb03e5a9206db7ddb81aa6e9b
Abhiraj Sawant <theabhirajsawant@gmail.com>
2026-01-12 07:09:32 +0530

    Day 04, Update 01

--- 001codewithHarry/03_chapter/03_01_learning.py
+++ 001codewithHarry/03_chapter/03_01_learning.py
@@ -1,6 +1,6 @@
 # ch3 STRINGS
-
-general_string = "Jerry"
+# we can't perform any change in a string
+general_string = "Jerry\\"
 print(general_string)
 # remember that there is no charater (char) data type in Python
 
@@ -8,7 +8,7 @@ print(general_string)
 
 name = "Mahendra Singh Dhoni"
 
-
+print(name[1]) #will print char at index 01
 print(name[:]) 
 
 # if there's nothing then it will be [0,len(name)+1] , its include the first index , but not the last index 
@@ -16,6 +16,7 @@ print(name[0:7])
 
 # here it will only go to the 6th indexing not the 7th, the indexing starts from 0.
 
+print(name[-8:-1])
 # to perform indexing from the end we can use negative numbers , its indexing will start from -1, -1 corresponds to '' len(name) - 1 ''
 print(name[::2]) # 2 is here the skip value 
 
@@ -24,7 +25,7 @@ print(name[::2]) # 2 is here the skip value
 
 #topic 02 , length of a string
 
-ex01 = "this is a string , 22, since numbers are written in strings , you cant find numbers here"
+ex01 = "this is a \'string\' , 22, since numbers are written in strings , you cant find numbers here"
 
 print(len(ex01))
 
---
+++ 001codewithHarry/03_chapter/03_02_questions.py
@@ -0,0 +1,32 @@
+# # question 01
+
+# username = input("writ eyour name: ")
+# print(f"good affternoon, \n\t {username}")
+
+# # question 02
+
+# letter = '''
+# Dear <|name|>,
+#     You are Seleted!
+#     Meet us on at <|date|>
+# '''
+# name = input("write your name: ")
+# date = input("choose your preferred date: ")
+
+# print(letter.replace("<|name|>", name).replace("<|date|>", date))
+
+
+#question 03 
+
+sample_text = "this is a semple test  so thatbwe cam find  double space occurance in this string"
+
+print(sample_text.find("  "))
+
+# question 04
+
+print(sample_text.replace("  "," "))
+
+# question 05
+
+sample_text2 = "this is a semple test,\n\tso thatbwe cam find double space occurance\n\tin this string"
+print(sample_text2)
